5f17ff2b492b4f6a5ab3f3dd22adeccf434d428c
Phil <cpmoviephil@gmail.com>
2021-03-22 14:16:47 +0800

    tts + pushy notify

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,188 @@
 import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
+import 'dart:async';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/services.dart';
+import 'package:pushy_flutter/pushy_flutter.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
-void main() {
-  runApp(MyApp());
+FlutterTts flutterTts = FlutterTts();
+
+void backgroundNotificationListener(Map<String, dynamic> data) async {
+  // Print notification payload data
+  print('Received notification: $data');
+
+  await textToSpeech(data['message']);
+
+  //playMusic(data['text_to_music']);
+  // Notification title
+  String notificationTitle = 'MyApp';
+
+  // Attempt to extract the "message" property from the payload: {"message":"Hello World!"}
+  String notificationText = data['message'] ?? 'Hello World!';
+
+  // Android: Displays a system notification
+  // iOS: Displays an alert dialog
+  Pushy.notify(notificationTitle, notificationText, data);
+
+  // Clear iOS app badge number
+  Pushy.clearBadge();
+}
+Future<void> textToSpeech(msg) async {
+  flutterTts.getVoices.then((value) => print(value));
+  await flutterTts.setSpeechRate(1.0);
+  await flutterTts.setVolume(1.0);
+  await flutterTts.setPitch(1.0);
+  await flutterTts.setLanguage("zh-TW");
+  await flutterTts.speak(msg);
+  //await flutterTts.setQueueMode(1);
+}
+Future<void> playMusic(url) async {
+  AudioPlayer audioPlayer = AudioPlayer();
+  int result = await audioPlayer.play(url);
+  if (result == 1) {
+    // success
+  }
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+void main() => runApp(Main());
+
+// Please place this code in main.dart,
+// After the import statements, and outside any Widget class (top-level)
+
+
+
+class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pushy',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        // Light theme with dark action bar
+        brightness: Brightness.light,
+        primaryColor: Colors.grey[900],
+        accentColor: Colors.redAccent,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: PushyDemo(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class PushyDemo extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _PushyDemoState createState() => _PushyDemoState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _PushyDemoState extends State<PushyDemo> {
+  String _deviceToken = 'Loading...';
+  String _instruction = '(please wait)';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  Future<void> playMusic(url) async {
+    AudioPlayer audioPlayer = AudioPlayer();
+    int result = await audioPlayer.play(url);
+    if (result == 1) {
+      // success
+    }
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method
+  Future<void> initPlatformState() async {
+    // Start the Pushy service
+    Pushy.listen();
+
+    try {
+      // Register the device for push notifications
+      String deviceToken = await Pushy.register();
+
+      // Print token to console/logcat
+      print('Device token: $deviceToken');
+
+      // Send the token to your backend server
+      // ...
+
+      // Update UI with token
+      setState(() {
+        _deviceToken = deviceToken;
+        _instruction =
+        Platform.isAndroid ? '(copy from logcat)' : '(copy from console)';
+      });
+    } on PlatformException catch (error) {
+      // Print to console/logcat
+      print('Error: ${error.message}');
+
+      // Show error
+      setState(() {
+        _deviceToken = 'Registration failed';
+        _instruction = '(restart app to try again)';
+      });
+    }
+
+    // Listen for push notifications received
+    Pushy.setNotificationListener(backgroundNotificationListener);
+
+    // Listen for push notification clicked
+    Pushy.setNotificationClickListener((Map<String, dynamic> data) {
+      // Print notification payload data
+      print('Notification clicked: $data');
+
+      // Extract notification messsage
+      String message = data['message'] ?? 'Hello World!';
+
+      // Display an alert with the "message" payload value
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+              title: Text('Notification clicked'),
+              content: Text(message),
+              actions: [
+                FlatButton(
+                  child: Text('OK'),
+                  onPressed: () {
+                    Navigator.of(context, rootNavigator: true).pop('dialog');
+                  },
+                )
+              ]);
+        },
+      );
+
+      // Clear iOS app badge number
+      Pushy.clearBadge();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // Demo app UI
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Pushy'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Builder(
+        builder: (context) => Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Text(_deviceToken,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            color: Colors.grey[700])),
+                  ),
+                  Text(_instruction,
+                      style: TextStyle(fontSize: 13, color: Colors.grey[600])),
+                ])),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
